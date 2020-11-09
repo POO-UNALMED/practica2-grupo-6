60be8439b83bf77e6eb3983c7ff43b2d0558ad26
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-11-09 16:25:28 -0500

    src

---
+++ src/parte1/VentanaInicio.java
@@ -0,0 +1,31 @@
+package parte1;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+
+
+
+
+public class VentanaInicio extends VBox{
+	
+	public VentanaInicio() {
+		super(10.0D);
+	}
+
+}
