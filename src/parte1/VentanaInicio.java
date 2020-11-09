bc0fbca1d5a162a7dc441c7f16201958cc244c76
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-11-09 17:04:50 -0500

    ventana basica

--- src/parte1/VentanaInicio.java
+++ src/parte1/VentanaInicio.java
@@ -3,6 +3,7 @@ package parte1;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
@@ -33,11 +34,37 @@ import javafx.scene.text.Text;
 public class VentanaInicio extends Application{
 	
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage primaryStage)  {
 		// TODO Auto-generated method stub
 		primaryStage.setTitle("Programa com GUI");
-			Scene myScene = new Scene(new Button("ingresar"),300,200);
-			primaryStage.setScene(myScene);
+			
+		
+		
+		
+			
+			
+		//gripane
+		GridPane p1 = new GridPane();
+		p1.setHgap(10);
+		p1.setAlignment(Pos.CENTER);
+		p1.add(new Button("bienvenido"), 0, 0);
+		
+		
+		//columna derecha
+		Button btn2 = new Button("h1");
+		Button btn3 = new Button("h2");
+		Button btn4 = new Button("h3");
+		VBox p2 = new VBox(btn2,btn3,btn4);
+		
+		//centro  controlador de los espacios
+		BorderPane p3 =new BorderPane();
+		p3.setRight(p2);		//derecha
+		p3.setTop(p1);			//arriba
+		
+		
+		
+		Scene myScene = new Scene(p3,300,200);
+		primaryStage.setScene(myScene);
 		
 		primaryStage.show();
 	}
