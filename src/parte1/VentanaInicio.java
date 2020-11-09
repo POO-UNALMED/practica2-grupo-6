ceb22cebe3e3c900ebc3e1473018488a5813fb88
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-11-09 16:43:01 -0500

    ventana

--- src/parte1/VentanaInicio.java
+++ src/parte1/VentanaInicio.java
@@ -1,13 +1,20 @@
 package parte1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
+
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -22,10 +29,23 @@ import javafx.scene.text.Text;
 
 
 
-public class VentanaInicio extends VBox{
-	
-	public VentanaInicio() {
-		super(10.0D);
-	}
 
+public class VentanaInicio extends Application{
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.setTitle("Programa com GUI");
+			Scene myScene = new Scene(new Button("ingresar"),300,200);
+			primaryStage.setScene(myScene);
+		
+		primaryStage.show();
+	}
+	
+	
+	public static void main(String[] args) {
+	    launch(args);
+	  }
+
+	
 }
