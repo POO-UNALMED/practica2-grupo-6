97420bb4904dd25f5169829faa46c59d50d5c537
dtorresag <55595673+dtorresag@users.noreply.github.com>
2020-11-25 16:22:01 -0500

    d

--- src/Excepciones/ErrorAplicacion.java
+++ src/Excepciones/ErrorAplicacion.java
@@ -1,9 +1,13 @@
 package Excepciones;
 
 public class ErrorAplicacion extends Exception {
-	String mensaje = "Manejo de errores de la Aplicación: ";
+	
+	
 	public ErrorAplicacion(String mensaje){
-		super(mensaje);
+		super("Manejo de errores de la Aplicación: "+"\n"+mensaje);
 	}
 	
 }
+	
+	
+
